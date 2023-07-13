dd9fd426b2f1ca3953dc718fffe6226dfffbfb2c
Clayton King <clayk@M1-MacBook-Pro.fios-router.home>
2023-07-12 22:17:28 -0400

    Initial Commit along with featured products page

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className='py-5 md:px-8 md:py-0 border-t bg-white'>
+      <div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row text-md'>
+        <p className='text-center leading-loose text-black md:text-left'>
+          &copy; 2023 UrbanGoods. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
